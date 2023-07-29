01e83634af805e6812b0660f921d832a9717d2b1
Robert <robertmaximus383@gmail.com>
2023-07-29 17:13:23 +0300

    layout updateto display users

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,13 +1,17 @@
 import Sidebar from "@/app/components/sidebar/Sidebar"
+import getUsers from "@/app/actions/getUsers";
+import UserList from "@/app/users/components/UserList";
 
 export default async function UsersLayout({
     children
 }: {
     children: React.ReactNode;
 }) {
+    const users = await getUsers();
     return(
         <Sidebar>
             <div className="h-full">
+                <UserList items={users}/>
                 {children}
             </div>
         </Sidebar>
