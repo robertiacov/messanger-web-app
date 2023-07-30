321581b9a8736b0728206c754353ad52a02cf873
Robert <robertmaximus383@gmail.com>
2023-07-30 21:12:09 +0300

    api route to send messages created

---
+++ app/api/messages/route.ts
@@ -0,0 +1,72 @@
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import { NextResponse } from "next/server";
+import prisma from "@/app/libs/prismadb"
+
+export async function POST(request: Request) {
+    try {
+       const currentUser = await getCurrentUser();
+       const body = await request.json();
+       const {
+        message,
+        image,
+        conversationId
+       } = body;
+
+       if(!currentUser?.id || !currentUser?.email) {
+        return new NextResponse('Unauthorized', {status: 401})
+       }
+
+       const newMessage = await prisma.message.create({
+        data:{
+            body: message,
+            image: image,
+            conversation:{
+                connect:{
+                    id: conversationId
+                },
+            },
+            sender: {
+                connect: {
+                    id: currentUser.id
+                }
+            },
+            seenBy: {
+                connect: {
+                    id: currentUser.id
+                }
+            }
+        },
+        include: {
+            seenBy: true,
+            sender: true,
+        }
+       });
+
+       const updateConversation = await prisma.conversation.update({
+            where:{
+                id: conversationId
+            },
+            data: {
+                lastMessageAt: new Date(),
+                messages:{
+                    connect:{
+                        id: newMessage.id
+                    }
+                }
+            },
+            include: {
+                users: true,
+                messages: {
+                    include: {
+                        seenBy: true
+                    }
+                }
+            }
+       });
+
+       return NextResponse.json(newMessage);
+    } catch (error: any) {
+        console.log(error, 'ERROR_MESSAGES');
+        return new NextResponse('Internal Error', {status: 500})
+    }
+}
