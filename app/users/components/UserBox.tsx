1cd4aa2a41af6300df956ca44203894e857b2622
Robert <robertmaximus383@gmail.com>
2023-07-30 20:27:40 +0300

    className fixed

--- app/users/components/UserBox.tsx
+++ app/users/components/UserBox.tsx
@@ -27,14 +27,30 @@ const UserBox: React.FC<UserBoxProps> = ({data}) => {
     }, [data, router]);
 
   return (
-    <div onClick={handleClick} className="w-full realtive flex items-center space-x-3 bg-white p-3 hover:bg-neutral-100 rounded-lg transition cursor-pointer">
-        <Avatar user={data}/>
+    <div
+        onClick={handleClick}
+        className="
+          w-full 
+          relative 
+          flex 
+          items-center 
+          space-x-3 
+          bg-white 
+          p-3 
+          hover:bg-neutral-100
+          rounded-lg
+          transition
+          cursor-pointer
+        "
+      >
+        <Avatar user={data} />
         <div className="min-w-0 flex-1">
-            <div className="focus:outline-none">
-                <div className="flex justify-between items-center mb-1">
-                    <p className="text-sm font-medium text-gray-900">
-                        {data.name}
-                    </p>
+          <div className="focus:outline-none">
+            <span className="absolute inset-0" aria-hidden="true" />
+            <div className="flex justify-between items-center mb-1">
+              <p className="text-sm font-medium text-gray-900">
+                {data.name}
+              </p>
                 </div>
             </div>
         </div>
