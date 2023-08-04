04db68ded33c5cd273e4d873dbd6bc1d3abac889
Robert <robertmaximus383@gmail.com>
2023-08-04 19:27:55 +0300

    feature: top loader added

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google'
 import ToasterContext from './context/ToasterContext'
 import AuthContext from './context/AuthContext'
 import ActiveStatus from './components/ActiveStatus'
+import NextTopLoader from "nextjs-toploader"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,6 +22,17 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <AuthContext>
+        <NextTopLoader
+          color="#0053FF"
+          // initialPosition={0.08}
+          crawlSpeed={200}
+          height={5}
+          crawl={true}
+          showSpinner={true}
+          easing="ease"
+          speed={200}
+          shadow="0 0 10px #0053FF,0 0 5px #0053FF"
+        />
           <ToasterContext />
           <ActiveStatus />
           {children}
