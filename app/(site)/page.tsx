4a5947ba8f364c5e46e72281a2a8e8094bac670e
Robert <robertmaximus383@gmail.com>
2023-08-07 22:26:59 +0300

    logo fix

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -7,13 +7,13 @@ export default function Home() {
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
             <Image 
                 alt="logo"
-                height="48"
-                width="48"
+                height="150"
+                width="150"
                 className="mx-auto w-auto"
                 src="/images/logo.png"
             />
             <h2
-            className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900"
+            className="text-center text-3xl font-bold tracking-tight text-gray-900"
             >
                 Sign in to your account
             </h2>
